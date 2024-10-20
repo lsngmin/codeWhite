2f63deb450f584996c266989c131382b63a03e5b
lsngmin <68813920+lsngmin@users.noreply.github.com>
2024-10-20 17:20:03 +0900

    모델 검증 데이터 세트 경로 설정 X fix

--- val.py
+++ val.py
@@ -9,6 +9,8 @@ path_g = path_lc if str(os.getcwd())[1] == 'U' else path_sv
 model_filename = path_g + "trainModel/tld_detection/weights/best.pt"
 model = YOLO(model_filename)
 
-metrics = model.val(batch=22, imgsz=1280, device="0,1")
+data = path_g + "tld_2024.yaml"
+
+metrics = model.val(data=data, batch=22, imgsz=1280, device="0,1")
 # metrics.box.map  # map50-95
 # metrics.box.map50  # map50
