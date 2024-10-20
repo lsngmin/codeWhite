1e538faaa2796dda5e03dc932c498352c48a59e1
lsngmin <68813920+lsngmin@users.noreply.github.com>
2024-10-20 19:44:48 +0900

    class13aug 매개값 재설정

--- augmentation/main.py
+++ augmentation/main.py
@@ -1,10 +1,10 @@
 # 클래스 별로 이미지 분류
 # 클래스 별 이미지 수량 출력
 from started.augmentation import classificationImage as cf
-
-cf.start()
+from started.utils import setPath as p
+#cf.start()
 
 # 해당 클래스의 이미지 증강 3번3
 from started.augmentation.clsimageaug import class13aug as c13
 for i in range(0,3):
-    c13.start()
+    c13.start(p.get_p("datasets")+"train_class/class_13/images", (p.get_p("datasets")+"train_class/class_13/labels"))
