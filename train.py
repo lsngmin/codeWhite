0490cc2e4edaebff0920101c47b517c86b3aada3
lsngmin <68813920+lsngmin@users.noreply.github.com>
2024-10-20 19:49:35 +0900

    train 경로 재설정

--- train.py
+++ train.py
@@ -9,5 +9,5 @@ gc.collect()
 torch.cuda.empty_cache()
 
 model = YOLO(p.get_p("trainModel/tld_detection/weights/best.pt"))
-model.train(cfg=p.get_p("commoms/cfg.yaml"))
+model.train(cfg=p.get_p("commons/cfg.yaml"))
 
