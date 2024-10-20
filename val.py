6ca392515482f4826fcb213c5e0b67eee9f6df45
lsngmin <68813920+lsngmin@users.noreply.github.com>
2024-10-20 17:16:16 +0900

    모델 경로 설정 에러 fix

--- val.py
+++ val.py
@@ -6,7 +6,7 @@ os.environ["NCCL_P2P_DISABLE"] = "1"
 path_lc = "/Users/smin/Desktop/ultralytics/codeWhite/"
 path_sv =  "/home/codeWhite/ultralytics/started/"
 path_g = path_lc if str(os.getcwd())[1] == 'U' else path_sv
-model_filename = path_g + "trainModel/best.pt"
+model_filename = path_g + "trainModel/tld_detection/weights/best.pt"
 model = YOLO(model_filename)
 
 metrics = model.val(batch=22, imgsz=1280, device="0,1")
