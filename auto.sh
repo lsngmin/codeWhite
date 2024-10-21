97b2de7a3a06dd54a901836b5a88a86ee5344771
lsngmin <68813920+lsngmin@users.noreply.github.com>
2024-10-21 14:53:23 +0900

    auto.sh 수정

--- auto.sh
+++ auto.sh
@@ -5,22 +5,21 @@ SOURCE_FOLDER="/home/codeWhite/ultralytics/started/trainModels/tld_detections_10
 TARGET_FOLDER="/home/codeWhite/ultralytics/started/rst/results.csv"
 
 # 파일 복사
-cp -r $SOURCE_FOLDER/* $TARGET_FOLDER
-
+cp $SOURCE_FOLDER $TARGET_FOLDER
 # Git 저장소로 이동
-cd $TARGET_FOLDER
+cd /home/codeWhite/ultralytics/started/
 
 # Git 상태 확인
 git status
 
 # Git 추가 (추가 또는 수정된 파일만)
-git add .
+git add $(basename $SOURCE_FOLDER)
 
 # 커밋 메시지 자동 생성
-git commit -m "Auto-commit at $(date)"
+git commit -m "Auto-commit file $(basename $SOURCE_FOLDER) at $(date)"
 
 # 원격 저장소에 푸시
 git push origin master  # main 브랜치로 푸시, 필요시 브랜치 이름 변경
 
 # 완료 메시지 출력
-echo "Files copied and pushed to repository at $(date)"
+echo "File $(basename $SOURCE_FOLDER) copied and pushed to repository at $(date)"
