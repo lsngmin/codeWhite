38d03f3956918dabd8e66bf86d14e86e8301c4c0
lsngmin <lsngmin@naver.com>
2024-10-22 15:05:01 +0900

    Auto-commit file results.csv at 2024. 10. 22. (화) 15:05:01 KST

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     img_files.sort() 
 
-    model_filename = p.get_p("/home/codewhite/ultralytics/started/trainModels/tld_detections_1021/weights/best.pt")
+    model_filename = p.get_p("trainModels/tld_detections_1021/weights/best.pt")
     model = YOLO(model_filename)
 
     for img_filename in img_files:
